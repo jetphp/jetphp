8579a556d2577f028a1d17d11edae0a8834e9020
João Artur <contato@joaoartur.com>
2018-01-19 23:25:05 -0200

    v0.5-dev

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
   /*
   * @author João Artur - www.joaoartur.com - www.github.com/JoaoArtur
   */
-
-  include 'vendor/autoload.php';
-  include 'app/principal/iniciar.php';
+  if (!file_exists('vendor/autoload.php')) {
+    echo "<p>Leia a <a href='https://github.com/JoaoArtur/JetPHP'>documentação do JetPHP</a> para aprender a instalar as dependências do framework!</p>";
+  } else {
+    include 'vendor/autoload.php';
+    include 'app/principal/iniciar.php';
+  }
 ?>
