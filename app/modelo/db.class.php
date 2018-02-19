17e68e6c609ef67f213f0aaf66936bffbc9edafc
João Artur <joaoartur>
2018-02-19 11:19:30 -0300

    db.class.php

--- app/modelo/db.class.php
+++ app/modelo/db.class.php
@@ -6,19 +6,44 @@
 
   // Modelo de bancos de dados
 
-  abstract class DB {
+  class DB {
     private static $conexao = null;
+    private static $qr      = null;
+
     private static function conectar() {
-      self::$conexao = new PDO('mysql:host='.Config::mostrar('DB_HOST').';charset=utf8;dbname='.Config::mostrar('DB_NOME'),Config::mostrar('DB_USUARIO'),Config::mostrar('DB_SENHA'));
+      if (self::$conexao != null) {
+        return true;
+      } else {
+        try {
+          self::$conexao = new PDO('mysql:host='.Config::mostrar('DB_HOST').';charset=utf8;dbname='.Config::mostrar('DB_NOME'),Config::mostrar('DB_USUARIO'),Config::mostrar('DB_SENHA'));
+          return true;
+        } catch (PDOException $e) {
+          echo "<p>Erro ao conectar no banco de dados: <b>".$e->getMessage()."</b></p>";
+          return false;
+        }
+      }
     }
 
+    public static function contar() {
+      return self::$qr->rowCount();
+    }
+    
+    public static function listar($tipo) {
+      return self::$qr->fetch($tipo);
+    }
+
+    public static function generico() {
+      return self::$qr;
+    }
+
+
     public static function executar($sql) {
-      if (self::$conexao == null) {
-        self::conectar();
+      if (self::conectar()) {
+        $qr = self::$conexao->prepare($sql);
+        $qr->execute();
+        self::$qr = $qr;
+        return new self;
       }
-      $qr = self::$conexao->prepare($sql);
-      $qr->execute();
-      return $qr;
     }
   }
 ?>
