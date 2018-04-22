c02a9972d6ac68ad64283350c0947c322e83b72a
João Artur <joaoartur>
2018-04-22 14:32:28 -0300

    [UPDATE] JetForms

--- app/modelo/jetforms.class.php
+++ app/modelo/jetforms.class.php
@@ -36,13 +36,16 @@
 
         public function loadForm() {
             $action = (Start::get('acao') == false) ? 'list' : Start::get('acao');
-            
+
             switch ($action) {
                 case 'list':
                     $this->listView();
                 break;
-
                 case 'edit':
+                    $this->editView();
+                break;
+
+                case 'delete':
 
                 break;
 
@@ -62,7 +65,7 @@
                 'action' => $allowed_actions
             ];
             $this->last_field = $name;
-            return $this;            
+            return $this;
         }
 
         public function addProperty($type,$value) {
@@ -132,6 +135,7 @@
                     foreach ($this->records[0] as $column=>$value) {
                         $this->t_header .= "<th>".$this->fields[$column]['title']."</th>";
                     }
+                $this->t_header .= "<th>Ação</th>";
                 $this->t_header .= "</tr>";
             }
             return $this;
@@ -139,15 +143,32 @@
 
         private function generateTableBody() {
             if (count($this->records) > 0) {
-                    foreach ($this->records as $record) {
+                    foreach ($this->records as $count=>$record) {
                         $this->t_body .= "<tr>";
                         foreach ($record as $field=>$value) {
                             $this->t_body .= "<td>".$value."</td>";
                         }
+
+                        $this->t_body .= $this->actionButtons($count+1);
                         $this->t_body .= "</tr>";
                     }
             }
             return $this;
+        }
+
+        private function actionButtons($id) {
+            $t = '<td>';
+            if (strstr($this->allowed_actions,'U')) {
+                // Check if Update is allowed
+                $t .= '<a href="?acao=edit&id='.$id.'" class="btn btn-success btn-circle"> <i class="fa fa-pencil"></i> </a>';
+            }
+            if (strstr($this->allowed_actions,'D')) {
+                // Check if Delete is allowed
+                $t .= '<a href="?acao=delete&id='.$id.'" class="btn btn-danger btn-circle"> <i class="fa fa-times"></i> </a>';
+            }
+            $t .= '</td>';
+
+            return $t;
         }
 
         public function generateTable() {
@@ -165,9 +186,12 @@
         // ------- End listView method -------
 
 
+
+
         // ------- addView method -------
         public function addView() {
             if ($this->query != false) {
+
             } else if ($this->table != false) {
                 $columns = [];
                 $f_columns = [];
@@ -207,7 +231,7 @@
                     <p>An error ocurred.</p>
                     <?php
                 }
-                
+
 
                 $this->render('add');
             } else {
@@ -227,7 +251,7 @@
                                 $f .= "<label>{$column['title']}:";
                                 if ($type == 'textarea') {
                                     $f.= "<textarea name='{$column['name']}' {$class} max='{$column['max']}'></textarea>";
-                                } else {                                
+                                } else {
                                     $f.= "<input name='{$column['name']}' type='{$type}' {$class} max='{$column['max']}'>";
                                 }
                                 $f .= "</label>";
@@ -243,8 +267,111 @@
             <?php
             }
         }
-        
+
         // ------- End addView method -------
+
+
+
+        // ------- Begin editView -------
+
+        public function editView() {
+
+            if (Start::get('id') != '' && Start::get('id') != false && Start::get('id') != null) {
+                if ($this->query != false) {
+
+                } else if ($this->table != false) {
+                    $sdb = new StaticDB();
+                    $tbl = $sdb->{$this->table};
+                    $edt = $tbl()->show(Start::get('id'));
+
+                    $columns = [];
+                    $f_columns = [];
+                    $qr = DB::execute("SELECT DISTINCT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_NAME = :table", ['table'=>$this->table]);
+                    if ($qr->count() > 0) {
+                        while($r = $qr->list(PDO::FETCH_OBJ)) {
+                            $columns[] = $r->COLUMN_NAME;
+                        }
+                    }
+                    if (isset($columns) and count($columns) > 0) {
+                        $i = 0;
+                        foreach ($columns as $column) {
+                            if (isset($this->fields[$column])) {
+                                $qr = DB::execute("SELECT DISTINCT DATA_TYPE as type,CHARACTER_MAXIMUM_LENGTH as max FROM information_schema.COLUMNS WHERE TABLE_NAME = :t and COLUMN_NAME = '{$column}'", ['t' => $this->table]);
+                                $r  = $qr->list();
+
+                                $f_columns[$i] = [];
+                                $f_columns[$i]['title']  = $this->fields[$column]['title'];
+                                $f_columns[$i]['name']   = $this->fields[$column]['field'];
+                                $f_columns[$i]['action'] = str_split($this->fields[$column]['action']);
+                                $f_columns[$i]['value']  = (isset($edt->{$f_columns[$i]['name']})) ? $edt->{$f_columns[$i]['name']} : 'oi';
+
+                                if (isset($this->fields[$column]['class'])):
+                                    $f_columns[$i]['class'] = $this->fields[$column]['class'];
+                                endif;
+                                if (isset($this->fields[$column]['disabled'])):
+                                    $f_columns[$i]['disabled'] = $this->fields[$column]['disabled'];
+                                endif;
+                                $f_columns[$i]['type'] = $r->type;
+                                $f_columns[$i]['max']  = ($r->max != null) ? $r->max : 11;
+
+                                $i++;
+                            }
+                        }
+                        $this->f_columns = $f_columns;
+                    } else {
+                        ?>
+                        <p>An error ocurred.</p>
+                        <?php
+                    }
+
+
+                    $this->render('edit');
+                } else {
+
+                }
+            } else {
+                $this->render('error');
+            }
+
+            return $this;
+        }
+
+
+        private function generateEditFields() {
+            if (isset($this->f_columns) and count($this->f_columns) > 0) {
+                $f  = "<form method='post' action=''>";
+                    foreach ($this->f_columns as $column) {
+                        $field_value = (isset($this->fields[$column['name']]['value'])) ? $this->fields[$column['name']]['value'] : $column['value'];
+
+
+                        if (in_array('I', $column['action'])) {
+                            $type = $this->getFormType($column['type']);
+                            $class = (isset($column['class'])) ? 'class="'.$column['class'].'"' : '';
+                            $f .= "<div class='input-group'>";
+                                $f .= "<label>{$column['title']}:";
+                                if ($type == 'textarea') {
+                                    $f.= "<textarea name='{$column['name']}' {$class} max='{$column['max']}'>".$field_value."</textarea>";
+                                } else {
+                                    $f.= "<input name='{$column['name']}' value='".$field_value."' type='{$type}' {$class} max='{$column['max']}'>";
+                                }
+                                $f .= "</label>";
+                            $f .= "</div>";
+                        }
+                    }
+                $f .= "</form>";
+
+                echo $f;
+            } else {
+            ?>
+            <p>An error ocurred.</p>
+            <?php
+            }
+        }
+
+        // ------- End editView -------
+
+
+
 
         private function getFormType($t) {
             switch ($t) {
@@ -263,15 +390,19 @@
         private function render($type='list') {
             include 'app/visual/admin/inc/topo.phtml';
             include 'app/visual/admin/inc/menu.phtml';
-            
+
             // $add   = $this->add;
             $title = $this->title;
             include 'app/visual/admin/inc/_base.phtml';
-            
+
             if ($type == 'list') {
                 $this->generateTable();
             } else if ($type == 'add') {
                 $this->generateAddFields();
+            } else if ($type == 'edit') {
+                $this->generateEditFields();
+            }  else if ($type == 'error') {
+                echo "<p>An error ocurred.</p>";
             } else {}
 
             include 'app/visual/admin/inc/rodape1.phtml';
