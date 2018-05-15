12437a2f53d90706ee92319418eaa30fc9ade2ee
João Artur <contato@joaoartur.com>
2018-05-15 17:01:08 -0300

    Update controller call

--- app/controlador/InicioControle.php
+++ app/controlador/InicioControle.php
@@ -1,16 +1,11 @@
 <?php
-  /*
-  * @author João Artur
-  * @description www.joaoartur.com - www.github.com/JoaoArtur
-  */
-
   class InicioControle extends Controle {     
     public static function index() {
       SEO::add('titulo','Início - JetPHP');
       SEO::add('descricao','JetPHP é um framework simples, cujo cunho é o desenvolvimento de sites e sistemas e geral.');
       SEO::add('keywords','jetphp, joao artur');
 
-      return Controle::view('site.inicio');
+      return self::view('site.inicio');
     }
   }
 ?>
