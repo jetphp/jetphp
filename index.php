e94d7a0fe0f43bc11cd738f67e276b45f33b99e8
João Artur <joaoartur>
2018-05-17 23:03:44 -0300

    Separate public path from framework source

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  /*
-  * @author João Artur - www.joaoartur.com - www.github.com/JoaoArtur
-  */
-  if (file_exists('vendor/autoload.php')) {
-    include 'vendor/autoload.php';
-  }
-  include 'app/principal/iniciar.php';
-?>
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+  if (file_exists('../vendor/autoload.php')) {
+    include '../vendor/autoload.php';
+  }
+  include '../app/main/main.php';
+?>
