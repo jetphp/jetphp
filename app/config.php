20a6e09283060461f993b334291826ff67b845ae
João Artur <contato@joaoartur.com>
2018-08-08 14:33:57 -0300

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,27 +1,25 @@
 <?php
-  \JetPHP\Model\Config::add('DEBUG',true);
-  \JetPHP\Model\Config::add('md5_salt','jetphp');
-  \JetPHP\Model\Config::add('TITULO','JetPHP');
-  \JetPHP\Model\Config::add('PASTA_PADRAO','/');
-  \JetPHP\Model\Config::add('PASTA_ADMIN','gerenciador/');
-  \JetPHP\Model\Config::add('PASTA_MISC',\JetPHP\Model\Config::show('PASTA_PADRAO').'misc/');
-  \JetPHP\Model\Config::add('ADMIN_MISC',\JetPHP\Model\Config::show('PASTA_PADRAO').'misc/manager/');
-  \JetPHP\Model\Config::add('GOOGLE_ANALYTICS','');
-  
-  // Manager tables
-  \JetPHP\Model\Config::add('ADMIN_SECTION','jet_sections');
-  \JetPHP\Model\Config::add('ADMIN_USERS','jet_users');
-  
+  use JetPHP\Model\Config;
+
+  Config::add('DEBUG',true);
+  Config::add('md5_salt','jetphp');
+  Config::add('TITULO','JetPHP');
+  Config::add('PASTA_PADRAO','/');
+  Config::add('PASTA_ADMIN','manager/');
+  Config::add('PASTA_MISC',Config::show('PASTA_PADRAO').'misc/');
+  Config::add('ADMIN_MISC',Config::show('PASTA_PADRAO').'misc/manager/');
+  Config::add('GOOGLE_ANALYTICS','');
+
   // Database configuration
-  \JetPHP\Model\Config::add('DB_HOST','localhost');
-  \JetPHP\Model\Config::add('DB_USUARIO','root');
-  \JetPHP\Model\Config::add('DB_SENHA','');
-  \JetPHP\Model\Config::add('DB_NOME','jetphp');
+  Config::add('DB_HOST','localhost');
+  Config::add('DB_USUARIO','root');
+  Config::add('DB_SENHA','');
+  Config::add('DB_NOME','jetphp');
   
   // E-mail configuration
-  \JetPHP\Model\Config::add('SMTP_HOST','smtp.exemplo.com');
-  \JetPHP\Model\Config::add('SMTP_USUARIO','usuario');
-  \JetPHP\Model\Config::add('SMTP_SENHA','senha');
-  \JetPHP\Model\Config::add('SMTP_EMAILPRINCIPAL','email@exemplo.com');
-  \JetPHP\Model\Config::add('SMTP_PORTA', 587);
+  Config::add('SMTP_HOST','smtp.exemplo.com');
+  Config::add('SMTP_USUARIO','username');
+  Config::add('SMTP_SENHA','password');
+  Config::add('SMTP_EMAILPRINCIPAL','example@example.com');
+  Config::add('SMTP_PORTA', 587);
 ?>
