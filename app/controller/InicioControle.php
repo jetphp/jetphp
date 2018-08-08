bd46c50710510f022f0ea0f8363804c061aa24f0
João Artur <contato@joaoartur.com>
2018-08-08 14:39:21 -0300

    Update InicioControle.php

--- app/controller/InicioControle.php
+++ app/controller/InicioControle.php
@@ -1,9 +1,10 @@
 <?php
+  use JetPHP\Model\SEO;
   class InicioControle extends Controle {     
     public static function index() {
-      \JetPHP\Model\SEO::add('titulo','Início - JetPHP');
-      \JetPHP\Model\SEO::add('descricao','JetPHP é um framework simples, cujo cunho é o \JetPHP\Model\desenvolvimento de sites e sistemas e geral.');
-      \JetPHP\Model\SEO::add('keywords','jetphp, joao artur');
+      SEO::add('titulo','Início - JetPHP');
+      SEO::add('descricao','JetPHP é um framework simples, cujo cunho é o \JetPHP\Model\desenvolvimento de sites e sistemas e geral.');
+      SEO::add('keywords','jetphp, joao artur');
 
       return self::view('site.inicio');
     }
