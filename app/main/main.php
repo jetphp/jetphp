2d3427327711bbcb4e0b4400fd64009dfed29b47
João Artur <contato@joaoartur.com>
2018-08-08 14:30:12 -0300

    Update main.php

--- app/main/main.php
+++ app/main/main.php
@@ -1,24 +1,20 @@
 <?php
   session_start();
-
   $phpv = phpversion();
   if ($phpv < 7) {
     echo "Your PHP version is lower than 7. Your version: $phpv";
     die;
   }
-
-  foreach (glob('../app/model/*.class.php') as $arquivo) {
-    include $arquivo;
+  foreach (glob('../app/model/*.class.php') as $file) {
+    include $file;
   }
-
   include '../app/main/jetphp.php';
   include '../app/config.php';
   include '../app/route.php';
+
   new \JetPHP\JetPHP;
 
   if (\JetPHP\Model\Config::show('DEBUG')) {
     ini_set('display_errors',1);
   }
-
-
 ?>
