3480137ebb0b613bae734f8f5d8500595904f046
João Artur <contato@joaoartur.com>
2019-02-28 09:13:37 -0300

    Update newdb.class.php

--- app/model/newdb.class.php
+++ app/model/newdb.class.php
@@ -1,3 +1,19 @@
-<?php
-
+<?php // not stable
+  class NewDB {
+    public static function getInstance() {
+      return new self;
+    } 
+    
+    public function __construct() {
+      $user = Config::show('DB_USUARIO');
+      $pass = Config::show('DB_USUARIO');
+      $db   = Config::show('DB_USUARIO');
+      $host = Config::show('DB_USUARIO');
+      try {
+        $con = new \PDO("mysql:host={$host};charset=utf8;dbname={$db}", $user, $pass);
+      } catch (\PDOException $e) {
+      
+      }
+    }
+  }
 ?>
