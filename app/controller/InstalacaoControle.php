b3a40ca495bebbc5f32f9a5996cba7769d33aeb8
Joao Artur <contato@joaoartur.com>
2019-07-02 17:23:14 -0300

    change composer config

--- app/controller/InstalacaoControle.php
+++ app/controller/InstalacaoControle.php
@@ -1,12 +1,12 @@
 <?php
   use JetPHP\Model\SEO;
 
-  class InstalacaoControle extends Controle {
+  class InstalacaoControle {
     public static function index() {
       SEO::add('titulo','Instalação - JetPHP');
       SEO::add('descricao','JetPHP é um framework simples, cujo cunho é o desenvolvimento de sites e sistemas e geral.');
       SEO::add('keywords','jetphp, joao artur');
-      return self::view('instalar.introducao');
+      return Load::view('instalar.introducao');
     }
   }
 ?>
