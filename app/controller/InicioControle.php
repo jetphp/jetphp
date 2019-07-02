38c17efed49d97be4774ca6f783298708127cde8
Joao Artur <contato@joaoartur.com>
2019-07-02 17:39:44 -0300

    bug fixes

--- app/controller/InicioControle.php
+++ app/controller/InicioControle.php
@@ -7,9 +7,9 @@
       SEO::add('keywords','jetphp, joao artur');
 
       $jl = new \JetPHP\Model\JetLoad();
-      $jl->setTop('inc.top');
+      $jl->setTop('inc.topo');
       $jl->setMenu('inc.menu');
-      $jl->setFooter('inc.footer');
+      $jl->setFooter('inc.rodape');
       $jl->view('site.inicio');
     }
   }
