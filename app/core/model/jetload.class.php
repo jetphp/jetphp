257a0de288ec9d319e1b2f6f5085629aa3c58522
Joao Artur <contato@joaoartur.com>
2019-07-02 17:38:19 -0300

    bug fixes jetload

--- app/core/model/jetload.class.php
+++ app/core/model/jetload.class.php
@@ -6,12 +6,17 @@
 		public $menu = false;
 		public $footer = false;
 		public $viewPath = '../app/view/';
+		public $vars;
 
 		public function view($path) {
 			$path = Format::convertPoints($path);
 			if (file_exists($this->viewPath.$path.'.phtml')) {
+				if ($this->vars != null) {
+					extract ($this->vars, EXTR_OVERWRITE);
+				}
 				if ($this->top != false && file_exists($this->viewPath.$this->top.'.phtml')) { include $this->viewPath.$this->top.'.phtml'; }
 				if ($this->menu != false && file_exists($this->viewPath.$this->menu.'.phtml')) { include $this->viewPath.$this->menu.'.phtml'; }
+
 				include $this->viewPath.$path.'.phtml';
 				if ($this->footer != false && file_exists($this->viewPath.$this->footer.'.phtml')) { include $this->viewPath.$this->footer.'.phtml'; }
 			} else {
@@ -26,6 +31,11 @@
 		/*
 		*	The view configuration $path syntax is: folder.name
 		*/
+		public function addVars($vars) {
+			if (is_array($vars)) {
+				$this->vars = $vars;
+			}
+		}
 		public function setMenu($path) {$this->menu = Format::convertPoints($path);return $this;}
 		public function setTop($path) {$this->top = Format::convertPoints($path);return $this;}
 		public function setFooter($path) {$this->footer = Format::convertPoints($path);return $this;}
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-  if (file_exists(__DIR__.'/vendor/autoload.php')) {
-    include __DIR__.'/vendor/autoload.php';
+  if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    include __DIR__.'/../vendor/autoload.php';
     include __DIR__.'/../app/core/autoload.php';
   } else {
     echo "You need to install dependencies using composer";
